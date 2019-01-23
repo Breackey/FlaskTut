c19b1a6a29b3eb188d34dd9e7803786e2fe81fdd
Breackey <brendaawino41@gmail.com>
2019-01-23 16:50:52 +0300

    explained code

--- hello.py
+++ hello.py
@@ -5,3 +5,4 @@ app=Flask(__name__) #instatiates flask class
 @app.route('/') #tells flask which url to trigger function
 def hello_world(): #the function to be trigered
   return "Hello World!"  #returns the message to be displayed
+
